7f3c8ad9ee6721dd9d3a97df9c18ad186ab6670a
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-06-09 18:53:11 +0200

    deleted LibraryFragment.kt

--- features/feature-library/src/main/java/dev/olog/feature/library/prefs/LibraryPreferences.kt
+++ features/feature-library/src/main/java/dev/olog/feature/library/prefs/LibraryPreferences.kt
@@ -5,18 +5,10 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import dev.olog.feature.library.R
 import dev.olog.feature.library.SpanCountController
-import dev.olog.navigation.screens.LibraryPage
 import dev.olog.feature.library.model.TabCategory
 import dev.olog.shared.android.extensions.observeKey
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
-
-private const val TAG = "AppPreferencesDataStoreImpl"
-
-private const val VIEW_PAGER_LAST_PAGE = "$TAG.VIEW_PAGER_LAST_PAGE"
-private const val VIEW_PAGER_PODCAST_LAST_PAGE = "$TAG.VIEW_PAGER_PODCAST_LAST_PAGE"
-
-
 
 internal class LibraryPreferences @Inject constructor(
     private val context: Context,
@@ -50,21 +42,4 @@ internal class LibraryPreferences @Inject constructor(
             putInt("${category}_span", spanCount)
         }
     }
-
-    fun getViewPagerLibraryLastPage(): Int {
-        return preferences.getInt(VIEW_PAGER_LAST_PAGE, 2)
-    }
-
-    fun setViewPagerLibraryLastPage(lastPage: Int) {
-        preferences.edit { putInt(VIEW_PAGER_LAST_PAGE, lastPage) }
-    }
-
-    fun getViewPagerPodcastLastPage(): Int {
-        return preferences.getInt(VIEW_PAGER_PODCAST_LAST_PAGE, 1)
-    }
-
-    fun setViewPagerPodcastLastPage(lastPage: Int) {
-        preferences.edit { putInt(VIEW_PAGER_PODCAST_LAST_PAGE, lastPage) }
-    }
-
 }
