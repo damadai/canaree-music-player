04c5402512805eeeeb6f7d22538fbc16cfa415cd
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-06-10 18:23:54 +0200

    created fragment arguments getter delegate

--- features/feature-presentation-base/src/main/java/dev/olog/feature/presentation/base/extensions/FragmentExtension.kt
+++ features/feature-presentation-base/src/main/java/dev/olog/feature/presentation/base/extensions/FragmentExtension.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 // TODO remove this??
 inline fun <T : Fragment> T.withArguments(vararg params: Pair<String, Any>): T {
@@ -32,3 +34,12 @@ fun Fragment.launchWhenResumed(block: suspend CoroutineScope.() -> Unit): Job {
     return viewLifecycleOwner.lifecycleScope.launchWhenResumed(block)
 }
 
+// can't be named arguments because it clashes with [Fragment.arguments]
+inline fun<reified T: Any> Fragment.argument(key: String): ReadOnlyProperty<Any?, T> {
+    return object : ReadOnlyProperty<Any?, T> {
+
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            return requireArguments().get(key) as T
+        }
+    }
+}
